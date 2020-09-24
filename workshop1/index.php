28edb8b874359d28b32dddc8deb3b667ff1b38de
gespinozac-utn <gespinozac@est.utn.ac.cr>
2020-09-24 11:32:44 -0600

    Add html
    Add html code to the file php, with some css lines.

--- workshop1/index.php
+++ workshop1/index.php
@@ -1,6 +1,22 @@
 <?php
     date_default_timezone_set("America/Costa_Rica"); // Set the default time zone, in this case Costa Rica.
     
-    echo 'Hoy es: ' . date("d/M/Y", time() ) . ', y son las ' . date("h:i A", time()); // print in the html
+    $fecha = 'Hoy es: ' . date("d/M/Y", time() ) . ', y son las ' . date("h:i A", time()); // print in the html
     // use date function to get the exact date and hour.
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Workshop 1 IWS-613</title>
+    <STYLE>
+        body{height: 50%; background-color:lightgrey;}
+        h1{position: fixed; padding: 10px; left: 20px; top: 30px}
+    </STYLE>
+</head>
+<body>
+    <h1> <?php echo $fecha ?> </h1>
+</body>
+</html>
